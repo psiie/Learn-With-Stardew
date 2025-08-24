3755021091ae78ccaef0bce28b6a317af71884bb
psiie <1077002+psiie@users.noreply.github.com>
2025-08-24 23:47:47 +0200

    refactored to common module

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const fs = require('fs');
-const path = require('path');
+const { loadLanguageSets } = require('./lib/common');
 
+/* ConcernedApe uses Magic-Strings to deliniate specific things in text, such as newline */
+function cleanLine(textLine) {
+  let cleanedLine = textLine;
+  cleanedLine = cleanedLine.replace(/#\$e#/g, '\n'); // Merge subsequent interactions as one dialogue
+  // cleanedLine = cleanedLine.replace(/\$b/g, '\n'); // break
+  cleanedLine = cleanedLine.replace(/\$\w/g, ''); // break
+  cleanedLine = cleanedLine.replace(/[#%*||^@]/g, ''); // aggressive lazy cleanup
 
-function loadLanguageSets(filenameBase) {
-  const en = fs.readFileSync(path.resolve(__dirname, 'unpacked/', `${filenameBase}.json`), 'utf-8')
-  const de = fs.readFileSync(path.resolve(__dirname, 'unpacked/', `${filenameBase}.de-DE.json`), 'utf-8')
-
-  return {
-    en: JSON.parse(en),
-    de: JSON.parse(de),
-  };
+  return cleanedLine;
 }
 
 function main() {
   const abigail = loadLanguageSets('Abigail');
   for (let key in abigail.en.content) {
-    console.log('\n', key)
-    console.log(abigail.en.content[key])
-    console.log(abigail.de.content[key])
+    const cardFront = cleanLine(abigail.de.content[key]); // target language
+    const cardBack = cleanLine(abigail.en.content[key]); // native loadLanguage
+
+    // console.log('\n', key)
+    console.log(
+      `\n\nKey: ${key}\n`,
+      cardFront,
+      '\n',
+      cardBack,
+    )
   }
 }
 
 main();
+
+// console.log(cleanLine("I can't trust a person who doesn't like animals...#$e#Oh, don't worry! I know you're an animal lover!$h"));
---
+++ items.js
@@ -0,0 +1,54 @@
+const { loadLanguageSets } = require('./lib/common');
+
+function pascalToTitleCase(str) {
+    return str
+        .replace(/_/g, ' ').trim() // clean up underscores
+        .replace(/([A-Z])/g, ' $1').trim() // pascal to title
+        .replace(/\s{2,}/g, ' ').trim() // reduce multiple spaces to one
+  }
+
+function generateCollection() {
+    const objects = loadLanguageSets('Objects');
+    const collection = {};
+
+    for (let key in objects.en.content) {
+        // console.log(key)
+
+        // If entry follows the pattern of a Name/Description pair, then process it as such
+        if (/_Name$/.test(key)) {
+            /* We dont know what the image name is thats on disk, so we calculate potential
+            names. Such as dev name, human name. Hopefully one exact-matches. */
+            const baseKey = key.replace(/_Name$/, '');
+            const descriptionKey = baseKey + '_Description';
+            const devName = pascalToTitleCase(key.replace(/_Name$/, ''));
+
+            const nameEN = objects.en.content[key];
+            const nameDE = objects.de.content[key]
+            
+            if (nameEN?.toLowerCase() === nameDE?.toLowerCase()) continue;
+
+            collection[baseKey] = {
+                name: {
+                    dev: devName,
+                    en: nameEN,
+                    de: nameDE,
+                },
+                description: {
+                    en: objects.en.content[descriptionKey]?.trim(),
+                    de: objects.de.content[descriptionKey]?.trim(),
+                }
+            }
+        }
+    }
+
+
+    return collection;
+}
+
+function main() {
+    const t = generateCollection();
+    console.log(t);
+    console.log(Object.keys(t).length);
+}
+
+main();
